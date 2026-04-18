69799b7eae7c50ca78c0c0eb36cb7a8f2805dfe2
lonely-wolf <3973152615@qq.com>
2026-04-18 22:15:18 +0800

    Update 蛊真人.py

--- 蛊真人.py
+++ 蛊真人.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+from concurrent.futures import ThreadPoolExecutor
+import threading
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0",
     "Referer": "https://m.zhaobiquge.com/",
@@ -11,35 +13,47 @@ headers = {
     "Upgrade-Insecure-Requests": "1"
 }
 encoding="utf-8"
-for n in range(1,49):
-    respond = requests.get(f"https://m.zhaobiquge.com/biquge/34/34104/index_{n}.html",headers=headers)
-    soup = BeautifulSoup(respond.text,"html.parser")
-    situations = soup.find_all("ul",class_="chapter")
-    situation = situations[0]
-    li = situation.find_all("li")
+def get_all_urls():
+        list = []
+        for n in range(1,49):
+            respond = requests.get(f"https://m.zhaobiquge.com/biquge/34/34104/index_{n}.html",headers=headers)
+            soup = BeautifulSoup(respond.text,"html.parser")
+            situations = soup.find_all("ul",class_="chapter")
+            situation = situations[0]
+            li = situation.find_all("li")
+            if li:
+                    for i in li:
+                        a=i.find("a")
+                        things=a.get("href")
+                        full_things="https://m.zhaobiquge.com/"+things
+                        list.append(full_things)
+        return list
+            
+
+def one_chapter(full_things):
+    print("当前线程：",threading.current_thread().name)
+    try:
+        in_respond = requests.get(full_things,headers=headers)    
+        final_soup = BeautifulSoup(in_respond.text,"html.parser")
+        div = final_soup.find("div",class_="reader-main")
+        h1_title = final_soup.find("div",class_="nr_title")
+        title = h1_title.get_text(strip=True)
+        word_read = final_soup.find("div", class_="nr_nr")
+        content = title + "\n"
+        if word_read:
+            para = word_read.find_all("p")
+            for j in para:
+                content += j.text + "\n" 
+        time.sleep(random.uniform(0.01,0.03))
+        return content
+    except Exception as e:
+        print("失败原因：", e)  
+        return ""
+
+if __name__=="__main__":
+    things = get_all_urls()
+    with ThreadPoolExecutor(max_workers=100)as executor:
+        result = executor.map(one_chapter,things)
     with open(r"E:\python\爬虫数据\蛊真人1.txt","a",encoding="utf-8")as f:
-        if li:
-            for i in li:
-                a=i.find("a")
-                things=a.get("href")
-                full_things="https://m.zhaobiquge.com/"+things
-                print(full_things)
-                try:
-                    in_respond = requests.get(full_things,headers=headers)    
-                    final_soup = BeautifulSoup(in_respond.text,"html.parser")
-                    div = final_soup.find("div",class_="reader-main")
-                    h1_title = final_soup.find("div",class_="nr_title")
-                    title = h1_title.get_text(strip=True)
-                    word_read = final_soup.find("div", class_="nr_nr")
-                    f.write(title)
-                    f.write("\n"*2)
-                    if word_read:
-                        para = word_read.find_all("p")
-                        for j in para:
-                            f.write(j.text)
-                            f.write("\n") 
-                            f.flush()
-                    print("\n"*8)
-                    time.sleep(random.randint(0,3))
-                except Exception as e:
-                    print("失败原因：", e)
+        for content in result:
+            f.write(content)
